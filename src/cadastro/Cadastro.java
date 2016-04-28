d0bc946281f58435b9eac7cbf19d70fb6478a1e1
IHMHR <martinelli.igor@hotmail.com>
2016-04-28 01:56:25 -0300

    Algumas classes
    Começando a implementação do sistema, algumas classes já criadas

---
+++ src/cadastro/Cadastro.java
@@ -0,0 +1,13 @@
+/**
+ * Classe para implementação do Cadastro
+ */
+package cadastro;
+
+/**
+ * @author Martinelli
+ * @version 1.0.0.0
+ * @category Acadêmico
+ */
+public class Cadastro {
+
+}
---
+++ src/erros/SisVendasException.java
@@ -0,0 +1,22 @@
+/**
+ * Classe para implementação da Tratativa de Erros
+ */
+package erros;
+
+/**
+ * @author Martinelli
+ * @version 1.0.0.0
+ * @category Acadêmico
+ */
+public class SisVendasException extends Exception {
+
+	public SisVendasException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public SisVendasException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	/*}*/
+}
